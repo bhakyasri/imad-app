29874e4e9a039b619aa2fdb63d2d329b9bb1f5c8
bhakyasri <30617990+bhakyasri@users.noreply.github.com>
2017-08-07 01:50:49 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,9 @@
 console.log('Loaded!');
+var marginLeft= 0;
+function moveRight(){
+    marginLeft = marginLeft + 1;
+    img.style.marginLeft = marginLeft + 'px' ;
+}
+img.onclick = function (){
+   var interval = setInterval(moveRight,50);
+}
