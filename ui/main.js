2115f2088f93dba16769d3fbe9cacf27af42b8fb
bhakyasri <30617990+bhakyasri@users.noreply.github.com>
2017-08-08 20:29:34 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -7,4 +7,20 @@ function moveRight(){
 }
 img.onclick = function (){
    var interval = setInterval(moveRight,50);
-}
+};
+button.onclick = function(){
+    //create a request object
+   var request = new XMLHttpRequest();
+   //create a response and store it as variable
+   request.onreadystatechange = function(){
+       if(request.readyState === XMLHttpRequest.DONE){
+           if(request.status === 200){
+               var counter = request.responseText();
+               var span = document.getElementById("counter");
+               span.InnerHTML = counter.toString();
+           }
+       }
+   }
+   request.open("GET","http://bhakya3.imad.hasura-app.io/counter",true);
+   request.send(null);
+};
