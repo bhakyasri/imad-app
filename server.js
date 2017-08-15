46017758b2c95a1d9be146fafd6975eb77da2bef
bhakyasri <30617990+bhakyasri@users.noreply.github.com>
2017-08-15 23:20:27 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -89,7 +89,7 @@ pool.query('SELECT * FROM test',function(err,result){
    if(err){
        res.status(500).send(err.tostring());
    } else{
-       res.send(JSON.stringify(result.rows));
+       res.send(JSON.stringify(result));
    }
     
 });
